670b35623ee6d852fea056a544fc29a3177b8b38
cb.romero.pizarro <cb.romero.pizarro@gmail.com>
2011-05-17 02:49:14 +0000

    Lista de Productos

--- webservices/webservice-producto/src/main/java/pe/edu/upc/dsd/ws/ProductoServiceImpl.java
+++ webservices/webservice-producto/src/main/java/pe/edu/upc/dsd/ws/ProductoServiceImpl.java
@@ -1,5 +1,8 @@
 package pe.edu.upc.dsd.ws;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.jws.WebService;
 
 import pe.edu.upc.dsd.ws.bean.Producto;
@@ -17,6 +20,39 @@ public class ProductoServiceImpl implements ProductoService{
 	    
 		return producto;
 
+	}
+
+	@Override
+	public List<Producto> obtenerListaProducto(String tipo) {
+		// TODO Auto-generated method stub
+		List<Producto> productos = new ArrayList<Producto>();
+		
+		Producto producto1 = new Producto();
+		producto1.setCodigo("P001");
+		producto1.setDescripcion("Cadenas");
+		producto1.setPrecio(250.05);
+		producto1.setTipo("Repuestos");
+		
+		productos.add(producto1);
+		
+		Producto producto2 = new Producto();
+		producto2.setCodigo("P002");
+		producto2.setDescripcion("Espejos");
+		producto2.setPrecio(250.05);
+		producto2.setTipo("Accesorios");
+		
+		productos.add(producto2);
+		
+		Producto producto3 = new Producto();
+		producto3.setCodigo("P003");
+		producto3.setDescripcion("LLantas");
+		producto3.setPrecio(250.05);
+		producto3.setTipo("Repuestos");
+		
+		productos.add(producto3);
+		
+		
+		return productos;
 	}
 	
 
