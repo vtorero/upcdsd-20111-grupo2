026f8df1d52d7618968984344b6f42abb2ed3d53
cb.romero.pizarro@gmail.com <cb.romero.pizarro@gmail.com@9a0251bc-e08e-6bb8-003b-1d6b7b1d272f>
2011-05-17 02:14:05 +0000

    Cliente para el WebService Producto

---
+++ trunk/webserviceclient-producto/src/main/java/pe/edu/upc/dsd/ws/ProductoService.java
@@ -0,0 +1,17 @@
+package pe.edu.upc.dsd.ws;
+
+import javax.jws.WebService;
+
+import pe.edu.upc.dsd.ws.bean.Producto;
+
+/**
+ * @author Carolina Romero P.
+ * 
+ */
+
+@WebService
+public interface ProductoService {
+	
+	public Producto obtenerProducto(String codigo);
+
+}
