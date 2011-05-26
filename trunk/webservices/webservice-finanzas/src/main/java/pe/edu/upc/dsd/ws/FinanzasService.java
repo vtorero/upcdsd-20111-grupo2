381e113f7d0e04e6a28dbb681eb9e41c75b6d081
fer.zegarrag@gmail.com <fer.zegarrag@gmail.com@9a0251bc-e08e-6bb8-003b-1d6b7b1d272f>
2011-05-26 10:17:34 +0000

    modificaciones Consulta de deuda vencida

--- trunk/webservices/webservice-finanzas/src/main/java/pe/edu/upc/dsd/ws/FinanzasService.java
+++ trunk/webservices/webservice-finanzas/src/main/java/pe/edu/upc/dsd/ws/FinanzasService.java
@@ -1,5 +1,27 @@
 package pe.edu.upc.dsd.ws;
 
+import java.util.List;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+import pe.edu.upc.dsd.ws.bean.DocumentoPendiente;
+
+@WebService
 public interface FinanzasService {
 
+	/**
+     * Obtiene la consulta deuda vencida.
+     * 
+     * @param codigoCliente
+	 * @return List<DocumentoPendiente>
+     */
+	public List<DocumentoPendiente> obtenerConsultaDeuda(@WebParam(name = "codigoCliente") String codigoCliente);
+	
+	/**
+     * Obtiene la consulta de la Línea de Credito del Cliente
+     * 
+     * @param codigoCliente
+	 * @return   double   
+     */
+	public double obtenerLineaCredito(@WebParam(name = "codigoCliente") String codigoCliente);
 }
--- trunk/webservices/webservice-finanzas/src/main/java/pe/edu/upc/dsd/ws/FinanzasServiceImpl.java
+++ trunk/webservices/webservice-finanzas/src/main/java/pe/edu/upc/dsd/ws/FinanzasServiceImpl.java
@@ -1,5 +1,52 @@
 package pe.edu.upc.dsd.ws;
+import java.util.ArrayList;
+import java.util.List;
+import javax.jws.WebService;
+import pe.edu.upc.dsd.ws.bean.DocumentoPendiente;
 
-public class FinanzasServiceImpl {
+@WebService(endpointInterface = "pe.edu.upc.dsd.ws.FinanzasService")
+public class FinanzasServiceImpl implements  FinanzasService{
+	
+	/**
+     * @see pe.edu.upc.dsd.ws.FinanzasService#obtenerConsultaDeuda(java.lang.String)
+     */
+    @Override
+    public List<DocumentoPendiente> obtenerConsultaDeuda(String codigoCliente)
+    {
+    	List<DocumentoPendiente> documentoPendientes = new ArrayList<DocumentoPendiente>();
+    	
+    	DocumentoPendiente documentoPendiente = new DocumentoPendiente();
+    	documentoPendiente.setNumDocumento("D04501");
+    	documentoPendiente.setTipoDocumento("Factura");
+    	documentoPendiente.setMonto(250.05);
+    	documentoPendiente.setMoneda("S/.");
+    	documentoPendiente.setDiasRetraso(10);
+    	documentoPendiente.setFechaEmision(null);
+    	documentoPendiente.setFechaVencimiento(null);
+    	
+    	documentoPendientes.add(documentoPendiente);
+    	    	
+    	documentoPendiente = new DocumentoPendiente();
+    	documentoPendiente.setNumDocumento("D02501");
+    	documentoPendiente.setTipoDocumento("Factura");
+    	documentoPendiente.setMonto(1400.05);
+    	documentoPendiente.setMoneda("$");
+    	documentoPendiente.setDiasRetraso(35);
+    	documentoPendiente.setFechaEmision(null);
+    	documentoPendiente.setFechaVencimiento(null);
+    	    	
+    	documentoPendientes.add(documentoPendiente);    	
+		
+        return documentoPendientes;
+    }
+    
+    /**
+     * @see pe.edu.upc.dsd.ws.FinanzasService#obtenerLineaCredito(java.lang.String)
+     */
+    @Override
+    public double obtenerLineaCredito(String codigo)
+    {
+        return 8000.00;
+    }
 
 }
