d717ef4b97181e9c2a799d7e4b79bfe3855e1749
adeza.villacorta <adeza.villacorta@gmail.com>
2011-05-27 01:38:13 +0000

    Ajustes

--- webservices-clients/webserviceclient-sv/src/main/java/pe/edu/upc/dsd/ws/PedidoService.java
+++ webservices-clients/webserviceclient-sv/src/main/java/pe/edu/upc/dsd/ws/PedidoService.java
@@ -1,5 +1,6 @@
 package pe.edu.upc.dsd.ws;
 
+import javax.jws.WebParam;
 import javax.jws.WebService;
 
 import pe.edu.upc.dsd.ws.bean.Pedido;
@@ -7,10 +8,11 @@ import pe.edu.upc.dsd.ws.bean.Cliente;
 
 @WebService
 public interface PedidoService {
-	public Pedido obtenerPedido(String codigo);
+    
+	public Pedido obtenerPedido(@WebParam(name = "codigo") String codigo);
 	
-	public String registrarPedido(Pedido pedido);
+	public String registrarPedido(@WebParam(name = "pedido") Pedido pedido);
 	
-	public Cliente consultarCliente(String codigoCliente);
+	public Cliente consultarCliente(@WebParam(name = "codigoCliente") String codigoCliente);
 	
 }
