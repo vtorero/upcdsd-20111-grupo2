ee03feff2be8627b52b7ecc275f14a3deedad7eb
ayala.olivares <ayala.olivares@gmail.com>
2011-06-15 23:45:06 +0000

    Se modifico test de servicio web de consultas de cliente

--- webservices-clients/webserviceclient-sv/src/test/java/pe/edu/upc/dsd/ws/ClienteServiceTest.java
+++ webservices-clients/webserviceclient-sv/src/test/java/pe/edu/upc/dsd/ws/ClienteServiceTest.java
@@ -1,17 +1,12 @@
 package pe.edu.upc.dsd.ws;
 
-
-
 import junit.framework.Assert;
 
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import pe.edu.upc.dsd.ws.bean.Pedido;
 import pe.edu.upc.dsd.ws.bean.Cliente;
-
-
 
 public class ClienteServiceTest {
 	
@@ -26,14 +21,14 @@ public class ClienteServiceTest {
 	@Test
 	public void testConsultarCliente()
     {
-		Cliente cliente  =  clienteService.consultarCliente("1");
-		Assert.assertEquals("1", cliente.getCodigo());
+		Cliente cliente  =  clienteService.consultarCliente("01");
+		Assert.assertEquals("01", cliente.getCodigo());
 		Assert.assertEquals("Carolina", cliente.getNombre());
 		Assert.assertEquals("Romero", cliente.getApellidoPaterno());
 		Assert.assertEquals("Pizarro", cliente.getApellidoMaterno());
 		Assert.assertEquals("Alameda los misioneros 664", cliente.getDireccion());
 		Assert.assertEquals("5752045", cliente.getTelefono());
-		Assert.assertEquals("1", cliente.getTipoPersona());
+		Assert.assertEquals("N", cliente.getTipoPersona());
     }
 
 }
