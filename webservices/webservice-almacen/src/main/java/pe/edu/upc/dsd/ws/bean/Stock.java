c9d0a20995134bec8a3cc332e8ac8405186ed599
cb.romero.pizarro <cb.romero.pizarro@gmail.com>
2011-06-16 20:23:01 +0000

    Actualizacion STOCK

---
+++ webservices/webservice-almacen/src/main/java/pe/edu/upc/dsd/ws/bean/Stock.java
@@ -0,0 +1,36 @@
+package pe.edu.upc.dsd.ws.bean;
+
+public class Stock {
+
+	private String codigoProducto;
+	private String descripcion;
+	private String stock;
+	private String almacen;
+	
+	public String getCodigoProducto() {
+		return codigoProducto;
+	}
+	public void setCodigoProducto(String codigoProducto) {
+		this.codigoProducto = codigoProducto;
+	}
+	public String getAlmacen() {
+		return almacen;
+	}
+	public void setAlmacen(String almacen) {
+		this.almacen = almacen;
+	}
+	
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public String getStock() {
+		return stock;
+	}
+	public void setStock(String stock) {
+		this.stock = stock;
+	}
+	
+}
