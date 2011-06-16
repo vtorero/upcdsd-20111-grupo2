73cff8629a6937e6781e18da0a1d1a9989cb2058
fer.zegarrag@gmail.com <fer.zegarrag@gmail.com@9a0251bc-e08e-6bb8-003b-1d6b7b1d272f>
2011-06-16 00:15:43 +0000

    Se añadio datos de prueba para obtener stocks de un producto

--- trunk/webservices/webservice-almacen/src/main/java/pe/edu/upc/dsd/ws/AlmacenServiceImpl.java
+++ trunk/webservices/webservice-almacen/src/main/java/pe/edu/upc/dsd/ws/AlmacenServiceImpl.java
@@ -1,16 +1,38 @@
 package pe.edu.upc.dsd.ws;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.jws.WebService;
 
 @WebService(endpointInterface = "pe.edu.upc.dsd.ws.AlmacenService")
-public class AlmacenServiceImpl implements AlmacenService {
-
+public class AlmacenServiceImpl implements AlmacenService 
+{
+	private Map<String, Integer> stocks;
+	
+	public AlmacenServiceImpl()
+	{
+		this.stocks = getStocksPrueba();
+	}
+	
 	/**
      * @see pe.edu.upc.dsd.ws.FinanzasService#import javax.jws.WebService(java.lang.String)
      */
 	@Override
     public int obtenerStock(String codigoProducto)
     {               
-    	return 100;
+    	return (int) this.stocks.get(codigoProducto);
     }
+	
+	private Map<String, Integer> getStocksPrueba()
+	{
+		Map<String, Integer> stocksPrueba = new HashMap<String, Integer>();
+		stocksPrueba.put("01", 100);
+		stocksPrueba.put("02", 150);
+		stocksPrueba.put("03", 800);
+		stocksPrueba.put("04", 250);
+		stocksPrueba.put("05", 80);
+		
+		return stocksPrueba;
+	}
 }
