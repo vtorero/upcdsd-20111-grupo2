91783b6fe7fdf88da9c8c0869cb79b8fa821acdd
fer.zegarrag <fer.zegarrag@gmail.com>
2011-06-16 01:40:21 +0000

    Se renombro paquetes. Tenia nombre errado

---
+++ sistema/src/main/java/pe/edu/upc/dsd/ws/ClienteService.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.dsd.ws;
+
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+import pe.edu.upc.dsd.ws.bean.Cliente;
+
+
+@WebService
+public interface ClienteService 
+{
+	public Cliente consultarCliente(@WebParam(name = "codigoCliente") String codigoCliente);
+}
