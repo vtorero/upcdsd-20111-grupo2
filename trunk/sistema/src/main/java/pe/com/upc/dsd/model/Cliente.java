b48bd8bfa90c69ded85fe93e6f5e0b07eadb89a7
wrequejo@gmail.com <wrequejo@gmail.com@9a0251bc-e08e-6bb8-003b-1d6b7b1d272f>
2011-06-16 01:12:32 +0000

    Se elimino paquete vacios

--- trunk/sistema/src/main/java/pe/com/upc/dsd/model/Cliente.java
+++
@@ -1,9 +0,0 @@
-package pe.com.upc.dsd.model;
-
-import java.io.Serializable;
-
-public class Cliente implements Serializable 
-{
-	private static final long serialVersionUID = 4508396085369172250L;
-
-}
--- trunk/sistema/src/main/java/pe/com/upc/dsd/model/Pedido.java
+++
@@ -1,9 +0,0 @@
-package pe.com.upc.dsd.model;
-
-import java.io.Serializable;
-
-public class Pedido implements Serializable 
-{
-	private static final long serialVersionUID = -6588881537613947670L;
-
-}
