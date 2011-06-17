2d1f78df61726a2cc24b871d24ec7c412360ba0a
adeza.villacorta <adeza.villacorta@gmail.com>
2011-06-17 00:15:27 +0000

    Se agrego campo de cantidad a bean de producto

--- webservices/webservice-producto/src/main/java/pe/edu/upc/dsd/ws/ProductoServiceImpl.java
+++ webservices/webservice-producto/src/main/java/pe/edu/upc/dsd/ws/ProductoServiceImpl.java
@@ -65,36 +65,42 @@ public class ProductoServiceImpl implements ProductoService
     	producto1.setDescripcion("HONDA XR 125");
     	producto1.setPrecio(860.00);
     	producto1.setTipo("01");
+    	producto1.setCantidad(0);
     	
     	Producto producto2 = new Producto();
     	producto2.setCodigo("02");
     	producto2.setDescripcion("LLANTAS HR21");
     	producto2.setPrecio(350.00);
     	producto2.setTipo("03");
+    	producto2.setCantidad(0);
 
     	Producto producto3 = new Producto();
     	producto3.setCodigo("03");
     	producto3.setDescripcion("YAMAHA XJ6-N");
     	producto3.setPrecio(1020.00);
     	producto3.setTipo("01");
+    	producto3.setCantidad(0);
     	
     	Producto producto4 = new Producto();
     	producto4.setCodigo("04");
     	producto4.setDescripcion("TUBO DE ESCAPE H500");
     	producto4.setPrecio(860.00);
     	producto4.setTipo("02");
+    	producto4.setCantidad(0);
     	
     	Producto producto5 = new Producto();
     	producto5.setCodigo("05");
     	producto5.setDescripcion("LLANTAS Z7G1");
     	producto5.setPrecio(480.00);
     	producto5.setTipo("03");
-
+    	producto5.setCantidad(0);
+    	
     	Producto producto6 = new Producto();
     	producto6.setCodigo("06");
     	producto6.setDescripcion("TIMON T823");
     	producto6.setPrecio(910.00);
     	producto6.setTipo("02");
+    	producto6.setCantidad(0);
     	
     	productos.add(producto1);
     	productos.add(producto2);
