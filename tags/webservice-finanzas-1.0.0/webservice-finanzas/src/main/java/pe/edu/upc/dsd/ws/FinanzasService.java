c17e3c7ff7b8dc98ca5f127f19909202baf61ace
adeza.villacorta@gmail.com <adeza.villacorta@gmail.com@9a0251bc-e08e-6bb8-003b-1d6b7b1d272f>
2011-06-17 00:45:13 +0000

    Release final de webservice finanzas

---
+++ tags/webservice-finanzas-1.0.0/webservice-finanzas/src/main/java/pe/edu/upc/dsd/ws/FinanzasService.java
@@ -0,0 +1,28 @@
+package pe.edu.upc.dsd.ws;
+
+import java.util.List;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+import pe.edu.upc.dsd.ws.bean.DocumentoPendiente;
+
+@WebService
+public interface FinanzasService {
+
+	/**
+     * Obtiene la consulta deuda vencida.
+     * 
+     * @param codigoCliente
+	 * @return List<DocumentoPendiente>
+     */
+	public List<DocumentoPendiente> getDocumentosPendientes(@WebParam(name = "codigoCliente") String codigoCliente);
+	
+	/**
+     * Obtiene la consulta de la Línea de Credito del Cliente
+     * 
+     * @param codigoCliente
+	 * @return   double   
+     */
+	public double obtenerLineaCredito(@WebParam(name = "codigoCliente") String codigoCliente);
+	
+}
---
+++ tags/webservice-finanzas-1.0.0/webservice-finanzas/src/main/java/pe/edu/upc/dsd/ws/bean/LineaCreditoDisponible.java
@@ -0,0 +1,14 @@
+package pe.edu.upc.dsd.ws.bean;
+
+public class LineaCreditoDisponible {
+	private double LineaCredito;
+	
+	public double getLineaCredito() {
+		return LineaCredito;
+	}
+	
+	public void setLineaCredito(double lineaCredito) {
+		LineaCredito = lineaCredito;
+	}
+
+}
