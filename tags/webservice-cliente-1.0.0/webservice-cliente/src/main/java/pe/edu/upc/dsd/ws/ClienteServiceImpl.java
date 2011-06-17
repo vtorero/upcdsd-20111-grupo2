d69d1a23bf0aecba4ad87fa59c252ad5e2cc6c5c
adeza.villacorta@gmail.com <adeza.villacorta@gmail.com@9a0251bc-e08e-6bb8-003b-1d6b7b1d272f>
2011-06-17 00:46:10 +0000

    Release final de webservice cliente

---
+++ tags/webservice-cliente-1.0.0/webservice-cliente/src/main/java/pe/edu/upc/dsd/ws/ClienteServiceImpl.java
@@ -0,0 +1,79 @@
+package pe.edu.upc.dsd.ws;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jws.WebService;
+
+import pe.edu.upc.dsd.ws.bean.Cliente;
+
+/**
+ * @author Andres Deza V.
+ * 
+ */
+@WebService(endpointInterface="pe.edu.upc.dsd.ws.ClienteService")
+public class ClienteServiceImpl implements ClienteService
+{
+	private List<Cliente> clientes;
+	
+	public ClienteServiceImpl()
+	{
+		this.clientes = getClientesPrueba();
+	}
+	
+	 @Override
+    public Cliente consultarCliente(String codigoCliente)
+    {
+		for (Cliente cliente : clientes) 
+		{
+			if(cliente.getDocumentoIdentidad().equals(codigoCliente))
+			{
+				return cliente;
+			}
+		}
+
+        return null;
+    }
+	 
+	private List<Cliente> getClientesPrueba()
+	{
+		List<Cliente> clientesPrueba = new ArrayList<Cliente>();
+		
+        Cliente cliente1 = new Cliente();
+        cliente1.setCodigo("01");
+        cliente1.setNombre("Carolina");
+        cliente1.setApellidoPaterno("Romero");
+        cliente1.setApellidoMaterno("Pizarro");
+        cliente1.setDireccion("Alameda los misioneros 664");
+        cliente1.setDocumentoIdentidad("1015987716");
+        cliente1.setTelefono("5752045");
+        cliente1.setTipoPersona("N");
+		
+        Cliente cliente2 = new Cliente();
+        cliente2.setCodigo("02");
+        cliente2.setNombre("Andres");
+        cliente2.setApellidoPaterno("Deza");
+        cliente2.setApellidoMaterno("Villacorta");
+        cliente2.setDireccion("Psje. Octavio Bernal 120 dpto F");
+        cliente2.setDocumentoIdentidad("44091088");
+        cliente2.setTelefono("4602787");
+        cliente2.setTipoPersona("N");        
+        
+        Cliente cliente3 = new Cliente();
+        cliente3.setCodigo("03");
+        cliente3.setNombre("Pedro");
+        cliente3.setApellidoPaterno("Torres");
+        cliente3.setApellidoMaterno("Diez");
+        cliente3.setDireccion("Av. La Molina 432");
+        cliente3.setDocumentoIdentidad("1044091088");
+        cliente3.setTelefono("4339812");
+        cliente3.setTipoPersona("N");
+        
+        
+        clientesPrueba.add(cliente1);
+        clientesPrueba.add(cliente2);
+        clientesPrueba.add(cliente3);
+        
+		return clientesPrueba;
+	}
+}
